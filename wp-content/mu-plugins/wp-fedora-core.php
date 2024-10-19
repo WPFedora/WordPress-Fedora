990ebd598c650d7e26506c243aaab925f9020faf
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-10-18 22:52:12 -0400

    Admin Footer Customizer enabled by default

--- wp-content/mu-plugins/wp-fedora-core.php
+++ wp-content/mu-plugins/wp-fedora-core.php
@@ -71,6 +71,9 @@ function wp_fedora_set_default_options() {
     if ( get_option( 'wp_fedora_enable_dashboard_widgets' ) === false ) {
         update_option( 'wp_fedora_enable_dashboard_widgets', 0 ); // Dashboard Widgets disabled by default
     }
+    if ( get_option( 'wp_fedora_disable_admin_footer_customizer' ) === false ) {
+        update_option( 'wp_fedora_disable_admin_footer_customizer', 0 ); // Admin Footer Customizer enabled by default
+    }
 }
 add_action( 'init', 'wp_fedora_set_default_options' );
 
