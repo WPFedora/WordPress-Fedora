473e830e0423aa31cef562da1a78df4049093d0d
GoogleSEOMastermind <mew.schieler@gmail.com>
2024-10-20 23:27:12 -0700

    Update meta-editor.js
    Updated meta-editior.js to reflect dyanmic charachter counts below meta titles and meta descriptions in meta-boxes across all categories, tags, pages, posts and custom post types

--- wp-content/mu-plugins/wp-fedora/seo-tools/meta-editor.js
+++ wp-content/mu-plugins/wp-fedora/seo-tools/meta-editor.js
@@ -9,9 +9,17 @@ jQuery(document).ready(function ($) {
         });
     }
 
-    // Initialize character count updates
+    // Initialize character count updates for posts/pages/CPTs
     updateCharacterCount('wp_fedora_meta_title', 'meta_title_count');
     updateCharacterCount('wp_fedora_meta_description', 'meta_description_count');
     updateCharacterCount('wp_fedora_og_meta_title', 'og_meta_title_count');
     updateCharacterCount('wp_fedora_og_meta_description', 'og_meta_description_count');
+
+    // For taxonomies (categories/tags)
+    updateCharacterCount('wp_fedora_meta_title_tax', 'meta_title_count_tax');
+    updateCharacterCount('wp_fedora_meta_description_tax', 'meta_description_count_tax');
+
+    // For author archives
+    updateCharacterCount('wp_fedora_meta_title_author', 'meta_title_count_author');
+    updateCharacterCount('wp_fedora_meta_description_author', 'meta_description_count_author');
 });
