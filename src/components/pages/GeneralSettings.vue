edf002367cd8273468119b0d1a6dc4056f6f305c
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-10-20 21:53:50 -0400

    setup dummy content for testing

--- src/components/pages/GeneralSettings.vue
+++ src/components/pages/GeneralSettings.vue
@@ -2,6 +2,33 @@
   <div id="general-settings-page" class="container-fluid ml-4">
     <h2 class="mb-4">General Settings</h2>
     <form novalidate @submit.prevent="submitHandler">
+      <div class="form-check form-switch-custom">
+        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+        <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+        <span class="slider"></span>
+        <div class="form-text">
+          Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special
+          characters, or emoji.
+        </div>
+      </div>
+      <div class="form-check form-switch-custom">
+        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+        <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+        <span class="slider"></span>
+        <div class="form-text">
+          Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special
+          characters, or emoji.
+        </div>
+      </div>
+      <div class="form-check form-switch-custom">
+        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+        <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+        <span class="slider"></span>
+        <div class="form-text">
+          Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special
+          characters, or emoji.
+        </div>
+      </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
