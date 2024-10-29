877d20ea540e758f2c195e4d0c14742800950255
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-10-29 01:13:06 -0400

    update footer text

--- src/php/ui/footer-customizer.php
+++ src/php/ui/footer-customizer.php
@@ -13,7 +13,7 @@ if ( !defined( 'ABSPATH' ) ) {
 
 // Customize the footer text in the WordPress admin
 function wp_fedora_custom_admin_footer_text() {
-    echo '<i><strong>WP Fedora</strong> brings your websites and SEO to life!</i>';
+    echo '<p><i><strong>WP Fedora</strong> brings your websites and SEO to life!</i> Got ideas to make WP Fedora better? Submit them on <a href="https://github.com/WPFedora/WordPress-Fedora/issues" target="_blank">our Github repo</a>.</p>';
 }
 add_filter('admin_footer_text', 'wp_fedora_custom_admin_footer_text');
 
